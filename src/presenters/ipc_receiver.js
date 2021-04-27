142cc3d1f66d29bf8461bb2c240ea81ba7770d4e
programadorLhama <programadorlhama@gmail.com>
2021-04-26 21:29:59 -0300

    a

--- src/presenters/ipc_receiver.js
+++ src/presenters/ipc_receiver.js
@@ -6,3 +6,5 @@ ipcMain.on('send-some-message', (event, args) => {
   console.log('Retornando uma mensagem assincrona!')
   event.reply('receive-asynchronous-reply', { bom: 'dia' })
 })
+
+console.log('Olam unS')
